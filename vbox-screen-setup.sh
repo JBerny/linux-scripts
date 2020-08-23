d36bd241063c29151de21abc465389a2b74fd307
jberny <f.bernardi89@gmail.com>
2020-08-23 13:33:36 +0200

    fixed vbox screen setup

--- vbox-screen-setup.sh
+++ vbox-screen-setup.sh
@@ -7,9 +7,19 @@ Y=$2
 #Refresh Rate in Hz, i.e. 60 
 RR=$3
 
+if test "X$1X" == "XX" || test "X$2X" == "XX" || test "X$3X" == "XX"; then 
+  echo "missing args"
+  exit 1
+fi
+
 CVTOUT=$(cvt $X $Y $RR)
-MODELINE=${CVTOUT/*Modeline/}
-MODE=$(echo $MODELINE | | cut -d' ' -f1)
-MONITOR=$(xrandr --query | grep " connected" | head -1 | cut -d' ' -f1)
+CVTOUT=${CVTOUT/*Modeline /}
+MODELINE=$( echo "$CVTOUT" | tr -d '"' )
+MODE=$(echo $MODELINE | cut -d' ' -f1)
+MONITOR=$(xrandr --query | grep " connected" | head -1 | cut -d' ' -f1 )
+echo "Found monitor $MONITOR"
+echo "Creating mode $MODELINE"
+xrandr --newmode ${MODELINE}
+echo "Resizing $MONITOR with $MODE"
 xrandr --addmode $MONITOR "$MODE"
 xrandr --output $MONITOR --mode "$MODE"
